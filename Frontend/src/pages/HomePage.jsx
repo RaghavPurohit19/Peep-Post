4123ac9517c7545e68aedf74e3b92ed37cd4a15c
ParwinderBaidwan <parwinderb550@gmail.com>
2024-09-07 22:23:53 +0530

    Added some functionality in HomePage, UserPage and LoginCard

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,7 +1,28 @@
 import { Button, Flex } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import {Link} from "react-router-dom";
+import useShowToast from "../hooks/useShowToast";
 
 const HomePage = () => {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const showToast = useShowToast();
+    useEffect(() => {
+        const getFeedPosts = async () => {
+            setLoading(true);
+            try {
+                const res = await fetch("/api/posts/feed");
+                const data = res.json();
+                console.log(data);
+            } catch (error) {
+                showToast("Error" , error.message, "error");
+            } finally {
+                setLoading(false);
+            }
+        };
+        getFeedPosts();
+    }, [showToast]);
+    
     return (
         <Link to = {"/markzuckerberg"}>
             <Flex w={"full"} justifyContent={"center"}>
